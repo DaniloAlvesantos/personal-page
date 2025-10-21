9991f21236657bfb71f6fc8c26f465463849c9eb
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2025-10-21 19:41:59 -0300

    add: skills

--- scripts/portifolio.js
+++ scripts/portifolio.js
@@ -1,3 +1,6 @@
+let skills;
+let projects;
+
 const getAge = () => {
   const birthAge = 2006;
   const currentYear = new Date().getFullYear();
@@ -6,3 +9,14 @@ const getAge = () => {
 };
 
 const age = (document.querySelector("#age").innerHTML = getAge() + " anos");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await fetch("api/data.json", {
+    method: "GET",
+  })
+    .then((d) => d.json())
+    .then((data) => data);
+
+  skills = response.skills;
+  projects = response.projects;
+});
